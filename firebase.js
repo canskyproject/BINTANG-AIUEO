a77d7c7cf660988a2b5f69c3554ee8c20fd1b0ec
canskyproject <135134830+canskyproject@users.noreply.github.com>
2026-06-28 19:37:01 +0700

    fix auth system & firebase.js

--- firebase.js
+++ firebase.js
@@ -1,132 +1,22 @@
 import { initializeApp }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import {
-getAuth,
-createUserWithEmailAndPassword,
-signInWithEmailAndPassword,
-signOut,
-updatePassword
-}
+import { getAuth }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import {
-
-getFirestore,
-doc,
-setDoc,
-getDoc,
-collection,
-getDocs,
-addDoc
-
-}
+import { getFirestore }
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-
-apiKey: "AIzaSyCwGlCnjTL3DfhbxWWYrQufV8_9uS7agQU",
-authDomain: "bintang-aiueo.firebaseapp.com",
-projectId: "bintang-aiueo",
-storageBucket: "bintang-aiueo.firebasestorage.app",
-messagingSenderId: "393513757252",
-appId: "1:393513757252:web:c3ecdd339ecb19f615ea11"
-
+  apiKey: "AIzaSyCwGlCnjTL3DfhbxWWYrQufV8_9uS7agQU",
+  authDomain: "bintang-aiueo.firebaseapp.com",
+  projectId: "bintang-aiueo",
+  storageBucket: "bintang-aiueo.firebasestorage.app",
+  messagingSenderId: "393513757252",
+  appId: "1:393513757252:web:c3ecdd339ecb19f615ea11"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+export const auth      = getAuth(app);
 export const firestore = getFirestore(app);
-
-window.loginUser = function(email,password){
-
-signInWithEmailAndPassword(
-auth,
-email,
-password
-)
-
-.then((userCredential) => {
-
-  const user =
-  userCredential.user;
-
-  localStorage.setItem(
-    "uid",
-    user.uid
-  );
-
-  localStorage.setItem(
-    "email",
-    user.email
-  );
-
-  alert("Login berhasil 😄");
-
-  window.location.href =
-  "dashboard.html";
-
-})
-
-.catch((error) => {
-
-alert(error.message);
-
-});
-
-};
-
-window.registerUser = function(email,password){
-
-createUserWithEmailAndPassword(
-auth,
-email,
-password
-)
-
-.then(async(userCredential) => {
-
-const user = userCredential.user;
-
-await setDoc(
-
-doc(
-firestore,
-"users",
-user.uid
-),
-
-{
-name:"User Baru",
-email:user.email,
-createdAt:new Date().toISOString()
-}
-
-);
-
-alert("Register berhasil 😄");
-window.location.href = "login.html";
-
-})
-
-.catch((error) => {
-
-alert(error.message);
-
-});
-
-};
-
-export {
-
-signOut,
-updatePassword,
-doc,
-setDoc,
-getDoc,
-collection,
-getDocs,
-addDoc
-
-};
